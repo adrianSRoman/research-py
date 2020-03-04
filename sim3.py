143c5c9458a71c5aae8c2aa4b2e6be86273965dd
AdrianSRoman <asroman@ucdavis.edu>
2020-03-04 13:22:18 -0800

    up to date sim3.py

--- sim3.py
+++ sim3.py
@@ -17,20 +17,23 @@ slower = 1/0.800 # Hz
 a = 1
 b = -1
 l1 = 2*2.7
-l2 = 1.0/1024
+l2 = 1.0/1000
 nlearn = 8
 z = (0.0+1j)*np.ones(time.shape)
 
 mean_slope = 0
+spr_cv = 0
+slopes = []
+cvs = []
 for i, f_s in enumerate([spr, faster, fast, slow, slower]):
 
 	x = np.exp(1j*2*np.pi*time*f_s)
 	x[int(nlearn*fs/f_s):] = 0
-	f = spr*np.ones(time.shape)
+	f = (spr+0.1*np.random.randn())*np.ones(time.shape)
 
 	for n, t in enumerate(time[:-1]):
 		z[n+1] = z[n] + (T*f[n])*(z[n]*(a + 1j*2*np.pi + b*(np.power(np.abs(z[n]),2))) + x[n])
-		f[n+1] = f[n] + T*(-np.abs(f[n]-f_s)*l1*(np.real(x[n]))*np.sin(np.angle(z[n])) - (f[n])*np.abs(spr-f[n])*l2*(f[n]-spr)/spr)
+		f[n+1] = f[n] + T*(-np.abs(f[n]-f_s)*l1*(np.real(x[n]))*np.sin(np.angle(z[n])) - f[n]*np.abs(spr-f[n])*l2*(f[n]-spr)/spr)
 	
 	print('###############################')
 	print('stimulus freq (Hz): ',f_s)
@@ -44,10 +47,24 @@ for i, f_s in enumerate([spr, faster, fast, slow, slower]):
 	peaks, _ = find_peaks(np.real(z[int((nlearn+1.5)*fs/f_s):]))
 	peaks = 1000*peaks/fs # converting to miliseconds
 	slope, _, _, _, _ = linregress(range(len(np.diff(peaks))), np.diff(peaks))
+	cv = np.std(np.diff(peaks))/np.mean(np.diff(peaks))
 	print('Slope: ', slope - mean_slope)
+	print('CV: ', cv)
 	#plt.plot(np.diff(peaks))
 	#plt.show()
 	#plt.clf
 	if i == 0:
 		mean_slope = slope
-		
+		spr_cv = cv
+	else:
+		slopes.append(slope-mean_slope)
+		cvs.append(cv)
+
+cvs.insert(2,spr_cv)
+plt.subplot(1,2,1)
+plt.bar(range(5),cvs)
+plt.subplot(1,2,2)
+plt.bar(range(4),slopes)
+plt.show()
+
+
